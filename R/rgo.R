8233dc7dbae9d02573c4e94d7bee8a0808894c5f
Yuya Matsumura <mattu.yuya@gmail.com>
2019-06-27 23:21:59 +0900

    add some functions

--- R/rgo.R
+++ R/rgo.R
@@ -6,3 +6,21 @@
 godouble <- function(x) {
   .Call("godouble", x, PACKAGE = "rigopractice")
 }
+
+#' Sum of Fibonacci Sequece
+#'
+#' @param x an integer
+#' @useDynLib rigopractice
+#' @export
+gofib <- function(x) {
+  .Call("gofib", x, PACKAGE = "rigopractice")
+}
+
+#' Fast Ver. Sum of Fibonacci Sequece
+#'
+#' @param x an integer
+#' @useDynLib rigopractice
+#' @export
+gofib_fast <- function(x) {
+  .Call("gofib_fast", x, PACKAGE = "rigopractice")
+}
